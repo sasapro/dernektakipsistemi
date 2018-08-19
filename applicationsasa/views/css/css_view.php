d532444ca3c1f0ee2848b77772166d62c3e95060
DESKTOP-8TS3S8U\sameds <samedsariaydin@gmail.com>
2018-08-19 18:30:37 +0300

    codeigniter ana klasör isimleri değiştirildi. Tema statik dosyaları yüklendi.

---
+++ applicationsasa/views/css/css_view.php
@@ -0,0 +1,69 @@
+
+body {background: #F1F1F1; font-family: 'Roboto', sans-serif; background: url('../images/bg.jpg') repeat;}
+/*body {background: #d2d6de; font-family: 'Roboto', sans-serif;}*/
+a, button, input, p, b, span {font-family: 'Roboto', sans-serif;}
+#main {transition: margin-left .5s;padding: 20px;padding-top: 100px;
+}
+h1.login-title {font-size: 36px; color: #666; text-align:center; margin: 30px auto; font-weight: 300;}
+h1.login-title strong {font-weight: 500;}
+h1.title {font-size: 26px; color: #666; padding: 10px 0px;}
+.user-login-logo {width: 70px; margin-bottom: auto; margin-top: 0px;margin-left:auto; margin-right:0;}
+/*.user-login-logo {margin: 30px auto; width: 240px; margin-top: 60px;}*/
+.user-login-logo img {width: 100%; max-width: 100%;}
+.card {border-radius: 10px 0px 10px 0px; border: 0; -webkit-box-shadow: 0 1px 15px 1px rgba(69,65,78,.08); box-shadow: 0 1px 15px 1px rgba(69,65,78,.08);}
+
+input, select {border:1px solid #ddd!important; border-radius: 10px 0 10px 0!important; background: #FBFBFB!important; font-family: 'Roboto', sans-serif;}
+.select2-container {width: 100%; max-width: 100%;}
+.select2-container--default .select2-selection--single  {height: 37px; padding: 5px; color: #ccc; border:1px solid #ddd!important; border-radius: 10px 0 10px 0!important; background: #FBFBFB!important; font-family: 'Roboto', sans-serif;}
+.select2-container--default .select2-selection--single .select2-selection__rendered {color: #999;}
+.select2-selection__arrow {padding: 5px;}
+.input-group input {border-radius: 0 0 10px 0!important;}
+.input-group .input-group-text {border-radius: 10px 0 0 0; background: #FBFBFB; }
+.form-group button,.form-group a {width: 100%; border-radius: 10px 0px 10px 0px; background: #0F6BAD; color: #fff!important;}
+.form-group label {color: #999; width: 100%;}
+
+.fixed-top {box-shadow: 0 1px 15px 1px rgba(69,65,78,.08);}
+
+.navbar-brand.logo {margin: 0; padding: 0;}
+.navbar-brand.logo img{height: 40px;}
+.mobile-menu-icon {cursor: pointer; color: #0F6BAD; font-size: 24px;}
+.mobile-menu-icon span {margin-left: 3px; }
+.navbar-light .navbar-nav .active > .nav-link, .navbar-light .navbar-nav .nav-link.active, .navbar-light .navbar-nav .nav-link.show, .navbar-light .navbar-nav .show > .nav-link {color: #0F6BAD;}
+
+.yayin-btn-group {margin: 20px 0; border-radius: 10px 0 10px 0;}
+.yayin-btn-group .btn {background: #fff; color: #999; border: 0;}
+.yayin-btn-group .btn:first-child {border-radius: 10px 0 0 0;}
+.yayin-btn-group .btn:last-child {border-radius: 0 0 10px 0;}
+.yayin-btn-group .btn.active {background-color: #0F6BAD!important;}
+.yayin-btn {margin: 20px 0; float: left; width: 170px; background-color: #0F6BAD; border-radius: 10px 0 10px 0;}
+
+.list-group label {font-size: 14px; color: #999; width: 100%;}
+
+.giris-btn {float: left; width: 180px; height: 140px; background: #fff; margin: 10px; }
+.giris-btn a {float: left; width: 100%;}
+.giris-btn i {float: left; width: 100%; font-size: 46px; text-align: center; padding: 20px 0;}
+.giris-btn span {float: left; width: 100%;font-size: 16px; text-align: center; padding:  0 10px;}
+
+.sidenav {height: 100%; width: 0; position: fixed; z-index: 1;top: 0;left: 0;background-color: #0F6BAD;overflow-x: hidden;padding-top: 60px;transition: 0.5s;}
+.sidenav a {padding: 8px 8px 8px 32px;text-decoration: none;font-size: 16px;color: #ffffff;display: block;transition: 0.3s;border-bottom: 1px solid #0E5D97;}
+.sidenav a:hover{color: #f1f1f1;}
+.sidenav .closebtn {position: absolute;top: 0;right: 25px;font-size: 36px;margin-left: 50px;}
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+}
+@media(min-width:992px) {
+    .mobile-menu-icon {display: none;}
+}
+
+.form-group .fa-times-circle {
+	display:none;
+	    position: absolute;
+    right: 12px;
+    top: 43px;
+	color: #0F6BAD;
+	cursor: pointer;
+}
+.cihaz {position: absolute;
+right: 0;
+top: -60px;}
---
+++ applicationsasa/views/js/js_view.php
@@ -0,0 +1,119 @@
+
+$(function(){
+	 //called when key is pressed in textbox
+  $('input[data-type=number]').keypress(function (e) {
+     //if the letter is not digit then display error and don't type anything
+     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+               return false;
+    }
+   });
+   
+   $(".form-group").each(function() {
+  
+  var $inp = $(this).find("input"),
+      $cle = $(this).find(".fa-times-circle");
+
+  $inp.on("input", function(){
+    $cle.toggle(!!this.value);
+  });
+  
+  $cle.on("touchstart click", function(e) {
+    e.preventDefault();
+    $inp.val("").trigger("input");
+  });
+  
+});
+   
+   
+});
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    $(".mobile-menu-icon").attr("onclick","closeNav()");
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    $(".mobile-menu-icon").attr("onclick","openNav()");
+}
+
+function ValidateIPaddress(ipaddress) {
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
+        return (true)
+    }
+    return (false)
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function validateUsername(username) {
+    var re = /^[a-zA-Z0-9]+$/;
+    return re.test(username);
+}
+
+
+
+function warningShow(a){
+    $(a).parent().find('.invalid-feedback').show();
+}
+function warningHide(a){
+    $(a).parent().find('.invalid-feedback').hide();
+}
+
+function formControl(){
+    var formData = "";
+    var i=0;
+    var required=0;
+    var type;
+    var value;
+    var datalength;
+    $(".required").each(function(){
+
+        value = $(this).val();
+        type = $(this).attr("data-type");
+        datalength = $(this).attr("data-length");
+
+        if(type=='username' && !validateUsername(value)){
+            warningShow(this);
+        }else if(type=='email' && !validateEmail(value)){
+            warningShow(this);
+        }else if(type=='number' && (isNaN(value) || value == "") || (datalength && datalength!=value.length) ){
+            warningShow(this);
+        }else if(type=='ip' && !ValidateIPaddress(value)){
+            warningShow(this);
+        }else if(value == ""){
+            warningShow(this);
+        }else{
+            warningHide(this);
+            required++;
+        }
+        i++;
+    });
+
+
+    if(i==required)
+        return true;
+    else
+        return false
+
+}
+
+function formAlert(status,message){
+	if(status==1){
+		$('.alert').html(message).prepend('<i class="fas fa-check-circle"></i> ').css('color','#155724').show();
+		setTimeout(function(){ $('.alert').hide(); }, 6000);
+	}else{
+		$('.alert').html(message).prepend('<i class="fas fa-times-circle"></i> ').css('color','#721c24').show();
+		setTimeout(function(){ $('.alert').hide(); }, 6000);
+	}
+}
+
+
+
